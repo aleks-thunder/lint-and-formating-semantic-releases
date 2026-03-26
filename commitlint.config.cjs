1522f2d3e6db9b5061e22c2641c9b1f00cd3d629
Oleksandr Moisieienko <o.moisieienko@lavinmedia.com>
2026-03-26 14:51:58 +0200

    feat(husky): implement husky

---
+++ commitlint.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // type-enum - commit prefix: "fix(...): ..."
+    "type-enum": [
+      2, // error level
+      "always", // always check
+      [
+        "feat",
+        "fix",
+        "chore",
+        "docs",
+        "ci",
+        "build",
+        "refactor",
+        "test",
+        "style",
+        "perf",
+        "revert",
+      ],
+    ],
+    "type-case": [2, "always", "lower-case"], // always check and convert to lower case
+  },
+};
