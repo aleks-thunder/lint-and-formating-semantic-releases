5e7eee6ae7c1ee6c467c8b030224c986e5831867
Oleksandr Moisieienko <o.moisieienko@lavinmedia.com>
2026-03-26 19:43:16 +0200

    feat(packages)!: add half-done configs

--- packages/base/eslint.js
+++ packages/base/eslint.js
@@ -1,13 +1,77 @@
-module.exports = {
-  root: true,
-  env: {
-    es2023: true,
-    node: true
+const eslint = require("@eslint/js");
+const unusedImports = require("eslint-plugin-unused-imports");
+const prettierPlugin = require("eslint-plugin-prettier");
+const simpleImportSort = require("eslint-plugin-simple-import-sort");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const prettierConfig = require("eslint-config-prettier");
+const tsParser = require("@typescript-eslint/parser");
+const eslintConfigPrettier = require("eslint-config-prettier");
+
+module.exports = [
+  eslint.configs.recommended,
+  eslintConfigPrettier,
+  prettierConfig,
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/coverage/**",
+    ],
+    languageOptions: {
+      ecmaVersion: 2025,
+      sourceType: "module",
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      "unused-imports": unusedImports,
+      "simple-import-sort": simpleImportSort,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "enum",
+          format: ["PascalCase"],
+        },
+        {
+          selector: "enumMember",
+          format: ["PascalCase", "UPPER_CASE"],
+        },
+      ],
+      "lines-between-class-members": [
+        "error",
+        {
+          enforce: [
+            { blankLine: "always", prev: "method", next: "method" },
+            { blankLine: "always", prev: "method", next: "field" },
+            { blankLine: "always", prev: "field", next: "method" },
+          ],
+        },
+      ],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "prettier/prettier": "error",
+      "prefer-const": "error",
+      "no-unused-expressions": "error",
+      "no-duplicate-imports": "error",
+      "object-shorthand": "warn",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
   },
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
-  },
-  extends: ["eslint:recommended"],
-  ignorePatterns: ["dist", "build", "coverage"]
-};
+];
--- packages/react/prettier.js
+++ packages/react/prettier.js
@@ -1,3 +1,20 @@
 module.exports = {
   ...require("@aleks-thunder/base/prettier"),
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+  singleAttributePerLine: true,
+  multilineArraysWrapThreshold: 2,
+  plugins: [
+    "prettier-plugin-multiline-arrays",
+    // Tailwind specific formatting
+    "prettier-plugin-tailwindcss",
+  ],
 };
