ca6c7a247de9cca0ef8cf45a6e54f2eafcef0160
Oleksandr Moisieienko <o.moisieienko@lavinmedia.com>
2026-04-07 17:16:05 +0300

    breaking: base major releast test

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,24 +1,25 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    // type-enum - commit prefix: "fix(...): ..."
-    "type-enum": [
-      2, // error level
-      "always", // always check
-      [
-        "feat",
-        "fix",
-        "chore",
-        "docs",
-        "ci",
-        "build",
-        "refactor",
-        "test",
-        "style",
-        "perf",
-        "revert",
-      ],
-    ],
-    "type-case": [2, "always", "lower-case"], // always check and convert to lower case
-  },
-};
+// module.exports = {
+//   extends: ["@commitlint/config-conventional"],
+//   rules: {
+//     // type-enum - commit prefix: "fix(...): ..."
+//     "type-enum": [
+//       2, // error level
+//       "always", // always check
+//       [
+//         "feat",
+//         "fix",
+//         "chore",
+//         "docs",
+//         "ci",
+//         "build",
+//         "refactor",
+//         "test",
+//         "style",
+//         "perf",
+//         "revert",
+//         "breaking",
+//       ],
+//     ],
+//     "type-case": [2, "always", "lower-case"], // always check and convert to lower case
+//   },
+// };
